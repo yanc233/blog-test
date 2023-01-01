7c48b0a1a6f96b68514640db3e9e6e6e9730afba
Yanc <yrc741897145@qq.com>
2023-01-01 23:52:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Author: Yanc
+ * @Date: 2023-01-01 23:48:31
+ * @LastEditTime: 2023-01-01 23:51:52
+-->
+<script setup lang="ts">
+  import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="测试一下使用github+vercel 自动发布部署" />
+</template>
+
+<style scoped>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.vue:hover {
+    filter: drop-shadow(0 0 2em #42b883aa);
+  }
+</style>
